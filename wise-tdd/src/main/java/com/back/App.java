2a1a56d2c3cce294ea048bc21ed8cd4fde0f5b3d
aovor <0hy2s2@naver.com>
2025-08-11 14:37:37 +0900

    등록 시 번호 자동 증가

--- wise-tdd/src/main/java/com/back/App.java
+++ wise-tdd/src/main/java/com/back/App.java
@@ -11,6 +11,7 @@ public class App {
 
     public void run() {
         System.out.println("== 명언 앱 ==");
+        int lastId = 0;
 
         while(true) {
             System.out.print("명령) ");
@@ -22,6 +23,10 @@ public class App {
                     String saying = sc.nextLine();
                     System.out.print("작가 : ");
                     String author = sc.nextLine();
+
+                    lastId++;
+
+                    System.out.println("%d번 명언이 등록되었습니다.".formatted(lastId));
                 }
                 case "종료" -> {
                     return;
--- wise-tdd/src/test/java/com/back/AppTest.java
+++ wise-tdd/src/test/java/com/back/AppTest.java
@@ -27,4 +27,33 @@ public class AppTest {
         assertThat(out).contains("명언 : ");
         assertThat(out).contains("작가 : ");
     }
+
+    @Test
+    @DisplayName("등록 시 번호 노출")
+    void t3() {
+        String out = AppTestRunner.run(
+                """
+                        등록
+                        현재를 사랑하라.
+                        작자미상
+                        """
+        );
+        assertThat(out).contains("1번 명언이 추가되었습니다.");
+    }
+
+    @Test
+    @DisplayName("등록 시 번호 노출")
+    void t4() {
+        String out = AppTestRunner.run(
+                """
+                        등록
+                        현재를 사랑하라.
+                        작자미상
+                        등록
+                        현재를 사랑하라.
+                        작자미상
+                        """
+        );
+        assertThat(out).contains("2번 명언이 등록되었습니다.");
+    }
 }
