d639837ae6a0e9066cf9a014a375f5a2df545a4f
aovor <0hy2s2@naver.com>
2025-08-11 14:49:58 +0900

    WiseSaying 생성, 목록 기능 추가

--- wise-tdd/src/main/java/com/back/App.java
+++ wise-tdd/src/main/java/com/back/App.java
@@ -1,5 +1,7 @@
 package com.back;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -12,6 +14,7 @@ public class App {
     public void run() {
         System.out.println("== 명언 앱 ==");
         int lastId = 0;
+        List<WiseSaying> wiseSayings = new ArrayList<>();
 
         while(true) {
             System.out.print("명령) ");
@@ -26,7 +29,20 @@ public class App {
 
                     lastId++;
 
+                    WiseSaying wiseSaying = new WiseSaying(lastId, saying, author);
+                    wiseSayings.add(wiseSaying);
                     System.out.println("%d번 명언이 등록되었습니다.".formatted(lastId));
+                }
+                case "목록" -> {
+                    System.out.print("번호 / 작가 / 명언");
+                    System.out.print("------------------");
+
+                    wiseSayings
+                            .reversed()
+                            .stream()
+                            .forEach(wiseSaying -> System.out.printf("%d / %s / %s%n",
+                                    wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getSaying()));
+
                 }
                 case "종료" -> {
                     return;
