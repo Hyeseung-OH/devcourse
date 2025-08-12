dc2ec51bf0ff948ec58e5e9e1b1f408c95278d98
aovor <0hy2s2@naver.com>
2025-08-12 11:55:20 +0900

    존재하지 않는 명언 수정 시 알림 메시지

--- wise-tdd/src/main/java/com/back/domain/wiseSaying/service/WiseSayingService.java
+++ wise-tdd/src/main/java/com/back/domain/wiseSaying/service/WiseSayingService.java
@@ -30,4 +30,8 @@ public class WiseSayingService {
     public boolean delete(int id) {
         return wiseSayingRepository.delete(id);
     }
+
+    public WiseSaying findByIdOrNull(int id) {
+        return wiseSayingRepository.findByIdOrNull(id);
+    }
 }
