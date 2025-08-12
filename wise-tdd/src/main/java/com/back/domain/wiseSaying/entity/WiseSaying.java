e18a7b7cd50d2db94986f9cb68d046b9961707c6
aovor <0hy2s2@naver.com>
2025-08-12 12:23:32 +0900

    lombok add, modify WiseSaying

--- wise-tdd/src/main/java/com/back/domain/wiseSaying/entity/WiseSaying.java
+++ wise-tdd/src/main/java/com/back/domain/wiseSaying/entity/WiseSaying.java
@@ -17,30 +17,6 @@ public class WiseSaying {
         this.author = author;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getSaying() {
-        return saying;
-    }
-
-    public void setSaying(String saying) {
-        this.saying = saying;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
     public boolean isNew() {
         return id == 0;
     }
