c09dbf230a26bf34a13f36f6c4364bb270237e1a
aovor <0hy2s2@naver.com>
2025-08-13 11:02:19 +0900

    file util 생성

---
+++ wise-tdd/src/main/java/com/back/standard/util/Util.java
@@ -0,0 +1,18 @@
+package com.back.standard.util;
+// 파일 관련된 것들을 다룰 예정
+
+public class Util {
+    // 이너 클래스
+    public static class file {
+        public static void touch(String filePath) {
+        }
+
+        public static boolean exists(String filePath) {
+            return false;
+        }
+    }
+
+    public static class json {
+
+    }
+}
