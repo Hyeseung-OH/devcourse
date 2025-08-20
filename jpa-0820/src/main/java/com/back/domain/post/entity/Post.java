7898f9a35c6e779fad8d41b4d676dd44a24c2d84
aovor <0hy2s2@naver.com>
2025-08-20 15:19:40 +0900

    post, initializing base data

---
+++ jpa-0820/src/main/java/com/back/domain/post/entity/Post.java
@@ -0,0 +1,23 @@
+package com.back.domain.post.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Entity
+public class Post {
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private int id;
+    private String title;
+    @Column(columnDefinition = "text")
+    private String content;
+}
