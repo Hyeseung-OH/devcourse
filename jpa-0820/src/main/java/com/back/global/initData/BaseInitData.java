e0b8cca57a2a2092646d9cda88b830b7407301e7
aovor <0hy2s2@naver.com>
2025-08-20 15:39:02 +0900

    postRepository는 JpaRepository를 상속받고 이 인터페이스는 JPA가 구현함

--- jpa-0820/src/main/java/com/back/global/initData/BaseInitData.java
+++ jpa-0820/src/main/java/com/back/global/initData/BaseInitData.java
@@ -1,15 +1,21 @@
 package com.back.global.initData;
 
+import com.back.domain.post.entity.Post;
+import com.back.domain.post.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BaseInitData {
+    @Autowired
+    private PostRepository postRepository;
+
     @Bean
     ApplicationRunner InitDataRunner() {
         return args -> {
-            System.out.println("Initializing base data...");
+            postRepository.count();
         };
     }
 }
