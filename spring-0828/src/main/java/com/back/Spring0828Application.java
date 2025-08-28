984ace6a04156f365e29daa6f3972921d0d3f240
aovor <0hy2s2@naver.com>
2025-08-28 12:49:10 +0900

    스프링부트, 컨트롤러, 빈, AOP 레포지토리

---
+++ spring-0828/src/main/java/com/back/Spring0828Application.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring0828Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring0828Application.class, args);
+    }
+
+}
