6fe01c313f0d24da2c5a30e1df5013102ecf4834
aovor <0hy2s2@naver.com>
2025-08-28 11:39:01 +0900

    질문22:  목록 URL 매핑

--- jsb-0827/src/main/java/com/mysite/sbb/TestInitData.java
+++ jsb-0827/src/main/java/com/mysite/sbb/TestInitData.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Profile;
 
 import java.time.LocalDateTime;
 
-@Profile("test")
+@Profile("dev")
 @Configuration
 @RequiredArgsConstructor
 public class TestInitData {
