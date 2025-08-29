9c4abcbadc4657467c0abba76306a15487b6abce
aovor <0hy2s2@naver.com>
2025-08-29 15:15:25 +0900

    19: CRUD 구현

--- spring-0828/src/main/java/com/back/domain/wiseSaying/controller/WiseSayingController.java
+++ spring-0828/src/main/java/com/back/domain/wiseSaying/controller/WiseSayingController.java
@@ -1,33 +1,44 @@
 package com.back.domain.wiseSaying.controller;
 
 import com.back.domain.wiseSaying.entity.WiseSaying;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Controller
 public class WiseSayingController {
 
-    private List<WiseSaying> wiseSayingList = new ArrayList<>();
+    private List<WiseSaying> wiseSayingList = new ArrayList<>() {{
+        add(new WiseSaying(1, "명언1", "작가1"));
+        add(new WiseSaying(2, "명언2", "작가2"));
+        add(new WiseSaying(3, "명언3", "작가3"));
+        add(new WiseSaying(4, "명언4", "작가4"));
+        add(new WiseSaying(5, "명언5", "작가5"));
+        add(new WiseSaying(6, "명언6", "작가6"));
+        add(new WiseSaying(7, "명언7", "작가7"));
+        add(new WiseSaying(8, "명언8", "작가8"));
+    }};
+
     private int lastId = 0;
 
     @GetMapping("/wiseSaying/write")
     @ResponseBody
-    public String write(
-            String content,
-            String author
-    ) {
-
+    public String write(String content, String author) {
         if(content == null || content.trim().length() == 0) {
-            throw new RuntimeException("명언을 입력해주세요.");
+            throw new RuntimeException("명언을 입력해 주세요.");
         }
 
         if(author == null || author.trim().length() == 0) {
-            throw new RuntimeException("작가를 입력해주세요.");
+            throw new RuntimeException("작가를 입력해 주세요.");
         }
 
         WiseSaying wiseSaying = new WiseSaying(++lastId, content, author);
@@ -39,7 +50,6 @@ public class WiseSayingController {
     @GetMapping("/wiseSaying/list")
     @ResponseBody
     public String list() {
-
         String wiseSayings = wiseSayingList.stream()
                 .map(w -> "<li>%s / %s / %s</li>".formatted(w.getId(), w.getContent(), w.getAuthor()))
                 .collect(Collectors.joining("\n"));
@@ -49,5 +59,36 @@ public class WiseSayingController {
                 %s
                 </ul>
                 """.formatted(wiseSayings);
+    }
+
+    @GetMapping("/wiseSaying/delete/{id}")
+    @ResponseBody
+    public String delete(@PathVariable int id) {
+        WiseSaying wiseSaying = findById(id);
+        wiseSayingList.remove(wiseSaying);
+
+        return "%d번 명언이 삭제되었습니다".formatted(id);
+    }
+
+    @GetMapping("/wiseSaying/modify/{id}")
+    @ResponseBody
+    public String modify(@PathVariable int id, @RequestParam(defaultValue = "기본값") String content, @RequestParam(defaultValue = "기본값") String author) {
+        WiseSaying wiseSaying = findById(id);
+        wiseSaying.setContent(content);
+        wiseSaying.setAuthor(author);
+
+        return "%d번 명언이 수정되었습니다.".formatted(wiseSaying.getId());
+    }
+
+    private WiseSaying findById(int id) {
+        Optional<WiseSaying> wiseSaying = wiseSayingList.stream()
+                .filter(w -> w.getId() == id)
+                .findFirst();
+
+        if(wiseSaying.isEmpty()) {
+            throw new RuntimeException("%d번 명언은 존재하지 않습니다.".formatted(id));
+        }
+
+        return wiseSaying.get();
     }
 }
