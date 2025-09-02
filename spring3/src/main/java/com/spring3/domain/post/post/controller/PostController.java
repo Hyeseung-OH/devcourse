1fbce6f97fe913d92d0a0cd290e492c9f1a1afb1
aovor <0hy2s2@naver.com>
2025-09-02 15:43:18 +0900

    20: 타임리프 캐시 끄고, @ModelAttribute(form) 추가하여 타임리프에서 writeForm 대신 form 사용

--- spring3/src/main/java/com/spring3/domain/post/post/controller/PostController.java
+++ spring3/src/main/java/com/spring3/domain/post/post/controller/PostController.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.stream.Collectors;
@@ -43,7 +44,7 @@ public class PostController {
 
     @PostMapping("/posts/doWrite")
     public String doWrite(
-            @Valid PostWriteForm form, BindingResult bindingResult, Model model
+            @ModelAttribute("form") @Valid PostWriteForm form, BindingResult bindingResult, Model model
     ) {
         if(bindingResult.hasErrors()) {
             // Stream 이용
