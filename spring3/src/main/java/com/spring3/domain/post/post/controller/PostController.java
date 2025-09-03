dfd39e5f23f747bcfc11bfb5211a798780e40377
aovor <0hy2s2@naver.com>
2025-09-03 14:04:32 +0900

    21-29: 게시글 등록 완성

--- spring3/src/main/java/com/spring3/domain/post/post/controller/PostController.java
+++ spring3/src/main/java/com/spring3/domain/post/post/controller/PostController.java
@@ -14,8 +14,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.stream.Collectors;
-
 @Controller
 public class PostController {
     private final PostService postService;
@@ -24,47 +22,34 @@ public class PostController {
         this.postService = postService;
     }
 
+
     @AllArgsConstructor
     @Getter
     public static class PostWriteForm {
-        @NotBlank(message = "1-제목을 입력해 주세요.")
-        @Size(min = 2, max = 10, message = "2-제목은 2글자 이상 10글자 이하로 입력해 주세요.")
+        @NotBlank(message = "01-title-제목을 입력해 주세요.")
+        @Size(min = 2, max = 10, message = "02-title-제목은 2글자 이상 10글자 이하로 입력해 주세요.")
         private String title;
 
-        @NotBlank(message = "3-내용을 입력해 주세요.")
-        @Size(min = 2, max = 100, message = "4-내용은 2글자 이상 100글자 이하로 입력해 주세요.")
+        @NotBlank(message = "03-content-내용을 입력해 주세요.")
+        @Size(min = 2, max = 100, message = "04-content-내용은 2글자 이상 100글자 이하로 입력해 주세요.")
         private String content;
     }
 
-
     @GetMapping("/posts/write")
-    public String write() {
+    public String write(@ModelAttribute("form") PostWriteForm form) {
         return "post/write";
     }
 
-    @PostMapping("/posts/doWrite")
+    @PostMapping("/posts/write")
     public String doWrite(
             @ModelAttribute("form") @Valid PostWriteForm form, BindingResult bindingResult, Model model
     ) {
         if(bindingResult.hasErrors()) {
-            // Stream 이용
-            String errorMessages = bindingResult.getFieldErrors()
-                    .stream()
-                    .map(field -> field.getField() + "-" + field.getDefaultMessage())
-                    .map(message -> message.split("-"))
-                    .map(bits -> """
-                            <!-- %s --><li data-error-field-name="%s">%s</li>
-                            """.formatted(bits[1], bits[0], bits[2]))
-                    .sorted()
-                    .collect(Collectors.joining("\n"));
-
-            model.addAttribute("errorMessages", errorMessages);
             return "post/write";
         }
 
         Post post = postService.write(form.title, form.content);
-
         model.addAttribute("id", post.getId());
-        return "post/writeDone";
+        return "redirect:/posts/write"; // 주소창을 바꿔
     }
 }
