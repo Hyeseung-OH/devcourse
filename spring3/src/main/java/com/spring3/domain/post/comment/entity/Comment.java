59601c6ccd64386a60d4806d181128ec7b31b995
aovor <0hy2s2@naver.com>
2025-09-04 10:35:47 +0900

    PostComment 엔티티 추가, 샘플 데이터도 생성, / OneToMany, ManyToOne

---
+++ spring3/src/main/java/com/spring3/domain/post/comment/entity/Comment.java
@@ -0,0 +1,22 @@
+package com.spring3.domain.post.comment.entity;
+
+import com.spring3.domain.post.post.entity.Post;
+import com.spring3.global.jpa.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Comment extends BaseEntity {
+    private String content;
+
+    // 연관관게 설정
+    // Many = Comment, One = Post
+    // Post 1개에 Comment 여러개가 달릴 수 있다.
+    // FK는 Comment 테이블에 post_id로 생성된다.
+    @ManyToOne
+    private Post post;
+}
--- spring3/src/main/java/com/spring3/domain/post/post/entity/Post.java
+++ spring3/src/main/java/com/spring3/domain/post/post/entity/Post.java
@@ -1,9 +1,13 @@
 package com.spring3.domain.post.post.entity;
 
+import com.spring3.domain.post.comment.entity.Comment;
 import com.spring3.global.jpa.entity.BaseEntity;
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @NoArgsConstructor
 @Getter
@@ -11,6 +15,15 @@ import lombok.NoArgsConstructor;
 public class Post extends BaseEntity {
     private String title;
     private String content;
+
+    // Cascade 속성은 트랜잭션 안에서만 작동함
+    // CascadeType.PERSIST: Post가 저장될 때 연관된 Comment들도 함께 저장
+    // CascadeType.REMOVE: Post가 삭제될 때 연관된 Comment들도 함께 삭제
+    // fetch = FetchType.LAZY: Post를 조회할 때 연관된 Comment들을 즉시 로드하지 않고, 실제로 접근할 때 로드
+    // mappedBy = "post": Comment 엔티티의 post 필드에 의해 매핑됨을 나타냄
+    @OneToMany(mappedBy = "post", cascade = {CascadeType.PERSIST, CascadeType.REMOVE}, fetch = FetchType.LAZY) // Comment.post 필드에 의해 매핑됨
+    // 비어 있는 값을 넣어 주지 않으면 NullPointerException이 발생할 수 있음
+    private List<Comment> comments = new ArrayList<>();
 
     public Post(String title, String content) {
         this.title = title;
@@ -20,5 +33,12 @@ public class Post extends BaseEntity {
     public void update(String title, String content) {
         this.title = title;
         this.content = content;
+    }
+
+    public Comment addComment(String content) {
+        Comment comment = new Comment(content, this);
+        this.comments.add(comment);
+
+        return comment;
     }
 }
