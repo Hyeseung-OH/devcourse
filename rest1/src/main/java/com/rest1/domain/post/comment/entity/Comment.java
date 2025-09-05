b754828bc27e73232079956aeb5f5f004b426b86
aovor <0hy2s2@naver.com>
2025-09-05 14:18:36 +0900

    1: REST API PROJECT

---
+++ rest1/src/main/java/com/rest1/domain/post/comment/entity/Comment.java
@@ -0,0 +1,28 @@
+package com.rest1.domain.post.comment.entity;
+
+import com.rest1.domain.post.post.entity.Post;
+import com.rest1.global.jpa.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Comment extends BaseEntity {
+    private String content;
+
+    // 연관관게 설정
+    // Many = Comment, One = Post
+    // Post 1개에 Comment 여러개가 달릴 수 있다.
+    // FK는 Comment 테이블에 post_id로 생성된다.
+    @ManyToOne
+    private Post post;
+
+    public void update(String content) {
+        this.content = content;
+    }
+}
