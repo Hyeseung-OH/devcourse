a454c1814bd77c09d1d577fc2789377c75d6f86d
aovor <0hy2s2@naver.com>
2025-09-08 12:29:53 +0900

    12~15: 댓글 삭제 ê 구현, 응답 전략으로 표준 코드에 커스터마이징 방식

--- rest1/src/main/java/com/rest1/domain/post/comment/controller/ApiV1CommentController.java
+++ rest1/src/main/java/com/rest1/domain/post/comment/controller/ApiV1CommentController.java
@@ -4,11 +4,15 @@ import com.rest1.domain.post.comment.dto.CommentDto;
 import com.rest1.domain.post.comment.entity.Comment;
 import com.rest1.domain.post.post.entity.Post;
 import com.rest1.domain.post.post.service.PostService;
+import com.rest1.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,7 +20,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApiV1CommentController {
     private final PostService postService;
 
+    @GetMapping("/{postId}/comments")
+    public List<CommentDto> getItems(
+            @PathVariable Long postId
+    ) {
+        Post post = postService.findById(postId).get();
+        return post.getComments().stream()
+                .map(CommentDto::new)
+                .toList();
+    }
+
     @GetMapping("/{postId}/comments/{commentId}")
+    @Transactional(readOnly = true)
     public CommentDto getItem(
             @PathVariable Long postId,
             @PathVariable Long commentId
@@ -24,5 +39,20 @@ public class ApiV1CommentController {
         Post post = postService.findById(postId).get();
         Comment comment = post.findCommentById(commentId).get();
         return new CommentDto(comment);
+    }
+
+    @GetMapping("/{postId}/comments/{commentId}/delete")
+    @Transactional
+    public RsData deleteItem(
+            @PathVariable Long postId,
+            @PathVariable Long commentId
+    ) {
+        Post post = postService.findById(postId).get();
+        postService.deleteComment(post, commentId);
+
+        return new RsData(
+                "204-1",
+                "%d번 댓글이 삭제되었습니다.".formatted(commentId)
+        );
     }
 }
