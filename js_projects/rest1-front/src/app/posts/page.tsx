186c76402760ceb0fdf3548c78c072d38634043e
aovor <0hy2s2@naver.com>
2025-09-12 15:14:23 +0900

    6~17: Next.js로 다건, 단건 조회

--- js_projects/rest1-front/src/app/posts/page.tsx
+++ js_projects/rest1-front/src/app/posts/page.tsx
@@ -1,3 +1,33 @@
+"use client";
+
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
 export default function Home() {
-    return <div>여기는 /posts 페이지입니다.</div>
+  const [posts, setPosts] = useState<{ id: number; title: string }[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8070/api/v1/posts")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setPosts(data);
+      });
+  }, []);
+
+  return (
+    <div className="flex flex-col gap-9">
+      <h1>글 목록</h1>
+      {posts.length === 0 && <div>Loading...</div>}
+      {posts.length > 0 && (
+        <ul>
+          {posts.map((post) => (
+            <li key={post.id}>
+              <Link href={`/posts/${post.id}`}>{post.id} : {post.title}</Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
