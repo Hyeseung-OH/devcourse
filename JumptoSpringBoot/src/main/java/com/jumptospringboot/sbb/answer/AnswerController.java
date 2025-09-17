a28379e5b6f1d55934092470f5119dc22767f030
aovor <0hy2s2@naver.com>
2025-09-17 22:59:28 +0900

    3-11 앵커 기능 추가

--- JumptoSpringBoot/src/main/java/com/jumptospringboot/sbb/answer/AnswerController.java
+++ JumptoSpringBoot/src/main/java/com/jumptospringboot/sbb/answer/AnswerController.java
@@ -44,8 +44,9 @@ public class AnswerController {
             model.addAttribute("question", question);
             return "question_detail";
         }
-        this.answerService.create(question, answerForm.getContent(), siteUser);
-        return String.format("redirect:/question/detail/%s", id);
+        Answer answer = this.answerService.create(question, answerForm.getContent(), siteUser);
+        return String.format("redirect:/question/detail/%s#answer_%s",
+                answer.getQuestion().getId(), answer.getId());
     }
 
     // 수정 폼을 보여 주기 위한 요청 -> 수정 버튼을 클릭, GET 요청으로 기존 데이터가 채워진 폼을 보여 줌
@@ -77,7 +78,7 @@ public class AnswerController {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정 권한이 없습니다.");
         }
         this.answerService.modify(answer, answerForm.getContent());
-        return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
+        return String.format("redirect:/question/detail/%s#answer_%s", answer.getQuestion().getId(), answer.getId());
     }
 
     // 답변 삭제 기능
@@ -99,6 +100,5 @@ public class AnswerController {
         Answer answer = this.answerService.getAnswer(id);
         SiteUser siteUser = this.userService.getUser(principal.getName());
         this.answerService.vote(answer, siteUser);
-        return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
-    }
+        return String.format("redirect:/question/detail/%s#answer_%s", answer.getQuestion().getId(), answer.getId());    }
 }
