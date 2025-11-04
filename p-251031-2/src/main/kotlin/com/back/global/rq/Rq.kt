4f716edd9e6c096c7dc52184053152392476029c
aovor <0hy2s2@naver.com>
2025-11-04 11:18:43 +0900

    dev

---
+++ p-251031-2/src/main/kotlin/com/back/global/rq/Rq.kt
@@ -0,0 +1,39 @@
+package com.back.global.rq
+
+class Rq(
+    cmd: String
+) {
+
+    val action: String
+    private val paramMap: Map<String, String>
+
+    init {
+
+        val cmdBits = cmd.split("?")
+        action = cmdBits[0]
+
+        paramMap = if (cmdBits.size == 2) {
+
+            cmdBits[1]
+                .split("&")
+                .map {  // aaa=bbb
+                    val paramBits = it.split("=")
+                    paramBits[0] to paramBits[1]
+                }
+                .toMap()
+        } else {
+            emptyMap()
+        }
+    }
+
+    private fun getParamValue(key: String): String? {
+        return paramMap[key]
+    }
+
+    fun getParamValueAsInt(key: String, defaultValue: Int): Int {
+        return getParamValue(key)
+            ?.toIntOrNull()
+            ?: defaultValue
+    }
+
+}
