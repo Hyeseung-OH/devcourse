2cb49c1f29fe21088ae2cdb2efcb13deee3d8e7f
aovor <0hy2s2@naver.com>
2025-11-04 09:19:04 +0900

    dev

---
+++ p-251031-2/src/main/kotlin/com/back/App.kt
@@ -0,0 +1,33 @@
+package com.back
+
+import com.back.domain.system.controller.SystemController
+import com.back.domain.wiseSaying.controller.WiseSayingController
+
+class App(
+    val wiseSayingController: WiseSayingController = WiseSayingController(),
+    val systemController: SystemController = SystemController()
+) {
+
+    fun run() {
+
+        println("== 명언 앱 ==")
+
+        while (true) {
+            print("명언) ")
+            val input = readln().trim()
+
+            val rq = Rq(input)
+
+            when (rq.action) {
+                "종료" -> {
+                    systemController.exit()
+                    break
+                }
+                "등록" -> wiseSayingController.write()
+                "목록" -> wiseSayingController.list()
+                "삭제" -> wiseSayingController.delete(rq)
+                "수정" -> wiseSayingController.modify(rq)
+            }
+        }
+    }
+}
